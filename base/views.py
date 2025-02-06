fbb0e4c8ff4f31f02e77d3cd302674cd600b3160
Aayush Sapkota <your-email@example.com>
2025-02-06 14:57:40 +0545

    Features Updates

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from django.contrib.auth.models import User
+from .models import Room,Topic
 from .forms import RoomForm
+from django.db.models import Q
 # Create your views here.
 
 # rooms=[
@@ -9,13 +11,32 @@ from .forms import RoomForm
 #     {'id':3,'name':'Lets learn react'},
 # ]
 
+def loginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        try:
+            user=User.objects.get(username=username)
+        except:
+            user=None
+
+        
+    context={}
+    return render(request,'base/login_register.html',context)
 
 
 
 def home(request):
-    rooms=Room.objects.all()
+    q=request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms=Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    topics=Topic.objects.all()
+    room_count=rooms.count()
     context={
-        'rooms':rooms
+        'rooms':rooms,'topics':topics,'room_count':room_count
     }
     return render(request, 'base/home.html',context)
 
@@ -48,3 +69,10 @@ def updateRoom(request,pk):
     
     context={'form': form}
     return render(request,'base/room_form.html',context)
+
+def deleteRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    if request.method=='POST':
+        room.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj' : room})
