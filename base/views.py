cbe119338d744632d6b5f76005726958f3568d96
Aayush Sapkota <your-email@example.com>
2025-02-08 15:36:22 +0545

    User Authentication Updated

--- base/views.py
+++ base/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Room,Topic
 from .forms import RoomForm
+from django.http import HttpResponse
 from django.db.models import Q
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
 # rooms=[
@@ -18,13 +22,22 @@ def loginPage(request):
         try:
             user=User.objects.get(username=username)
         except:
-            user=None
+            messages.error(request,'User does not exist')
 
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'Username OR password is incorrect')
         
     context={}
     return render(request,'base/login_register.html',context)
 
-
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q=request.GET.get('q') if request.GET.get('q') != None else ''
@@ -46,6 +59,7 @@ def room(request, pk):
     context={'room':room}
     return render(request,'base/room.html',context)
 
+@login_required(login_url='login')
 def createRoom(request):
     form=RoomForm()
     if request.method=='POST':
@@ -57,10 +71,14 @@ def createRoom(request):
     context={'form': form}
     return render(request,'base/room_form.html',context)
 
-
+@login_required(login_url='login')
 def updateRoom(request,pk):
     room=Room.objects.get(id=pk)
     form=RoomForm(instance=room)
+
+    if request.user != room.user:
+        return HttpResponse('You are not allowed here!')
+
     if request.method=='POST':
         form=RoomForm(request.POST,instance=room)
         if form.is_valid():
@@ -70,6 +88,7 @@ def updateRoom(request,pk):
     context={'form': form}
     return render(request,'base/room_form.html',context)
 
+@login_required(login_url='login')
 def deleteRoom(request,pk):
     room=Room.objects.get(id=pk)
     if request.method=='POST':
